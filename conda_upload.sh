6052546539e7a6db1088ffb087ff96f0d9e8eb64
MarcYin <marcollinbobo@gmail.com>
2019-01-29 17:56:00 +0000

    get filename

--- conda_upload.sh
+++ conda_upload.sh
@@ -6,7 +6,7 @@ conda config --set anaconda_upload no
 #export CONDA_BLD_PATH=~/conda-bld 
 #export VERSION=$SIAC_VERSION
 conda build . -c conda-forge -c jgomezdans
-export CONDA_PACKAGE=`conda build --output . | grep bz2`
+export CONDA_PACKAGE=`conda build --output . -c conda-forge -c jgomezdans| grep bz2`
 echo $CONDA_PACKAGE
-ls -lah $CONDA_BLD_PATH/noarch
+ls -lah $CONDA_PACKAGE
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_PACKAGE --force
