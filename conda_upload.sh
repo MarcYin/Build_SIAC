c2a39a699b7ce1a12699c2c04995f7222b97b513
MarcYin <marcollinbobo@gmail.com>
2019-01-29 17:16:25 +0000

    meta

--- conda_upload.sh
+++ conda_upload.sh
@@ -1,10 +1,10 @@
 #conda install conda-build  
 #conda install anaconda-client 
 PKG_NAME=siac && USER=f0xy  
-mkdir ~/conda-bld 
+#mkdir ~/conda-bld 
 conda config --set anaconda_upload no  
-export CONDA_BLD_PATH=~/conda-bld 
-export VERSION=$SIAC_VERSION
+#export CONDA_BLD_PATH=~/conda-bld 
+#export VERSION=$SIAC_VERSION
 conda build .
 export CONDA_PACKAGE=`conda build --output . | grep bz2`
 echo $CONDA_PACKAGE
