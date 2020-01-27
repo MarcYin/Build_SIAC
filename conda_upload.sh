31ccc74c6839dbb023c3aeace6d87cebf84fbabb
MarcYin <marcollinbobo@gmail.com>
2020-01-27 06:15:57 +0000

    updates

--- conda_upload.sh
+++ conda_upload.sh
@@ -2,8 +2,8 @@ conda install conda-build
 conda install anaconda-client 
 USER=f0xy  
 conda config --set anaconda_upload no  
-conda build . -c conda-forge -c jgomezdans
-export CONDA_PACKAGE=`conda build --output . -c conda-forge -c jgomezdans| grep bz2`
+conda build . -c conda-forge
+export CONDA_PACKAGE=`conda build --output . -c conda-forge| grep bz2`
 echo $CONDA_PACKAGE
 ls -lah $CONDA_PACKAGE
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_PACKAGE --force
